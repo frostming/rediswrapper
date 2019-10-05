4c1799d483f6c6ab2d10682d7b767ae3e4a69055
Frost Ming <mianghong@gmail.com>
2019-10-05 16:10:10 +0800

    Bump version to 0.3.0

--- rediswrapper/__init__.py
+++ rediswrapper/__init__.py
@@ -6,7 +6,7 @@ A pythonic wrapper for redis client.
 Author: Frost Ming<mianghong@gmail.com>
 """
 
-__version__ = '0.2.1'
-__all__ = ['RedisDict']
+__version__ = "0.3.0"
+__all__ = ["RedisDict"]
 
 from .storage import RedisDict
