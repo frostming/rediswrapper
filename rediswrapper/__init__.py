9ee51251a465ae8bc5f84a7c5888d544428acf78
Frost Ming <me@frostming.com>
2023-01-09 09:53:30 +0800

    fix: fix the requires-python range

--- rediswrapper/__init__.py
+++ rediswrapper/__init__.py
@@ -6,7 +6,7 @@ A pythonic wrapper for redis client.
 Author: Frost Ming<mianghong@gmail.com>
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.0.post1"
 __all__ = ["RedisDict"]
 
 from .storage import RedisDict
